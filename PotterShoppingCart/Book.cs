4b8886367418bd0d6b11627381950b115ecfa11b
jame2408 <jame2408@gmail.com>
2017-10-31 21:11:55 +0800

    Add a failed test case : When buy a book, price should be 100.

--- PotterShoppingCart/Book.cs
+++ PotterShoppingCart/Book.cs
@@ -2,5 +2,7 @@
 {
     public class Book
     {
+        public string ISBN { get; set; }
+        public int Count { get; set; }
     }
 }
--- PotterShoppingCart/PotterShoppingCartTest.cs
+++ PotterShoppingCart/PotterShoppingCartTest.cs
@@ -7,6 +7,7 @@ namespace PotterShoppingCart
     [TestClass]
     public class PotterShoppingCartTest
     {
+
         [TestMethod]
         public void No_Buy_Any_Books_Should_Be_0()
         {
@@ -14,5 +15,18 @@ namespace PotterShoppingCart
             var actual = target.GetPrice(new List<Book>());
             Assert.AreEqual(0, actual);
         }
+
+        [TestMethod]
+        public void When_Buy_First_Book_Should_Be_100()
+        {
+            var target = new Cart();
+            var books = new List<Book>()
+            {
+                new Book(){ ISBN = "1", Count = 1},
+            };
+            var actual = target.GetPrice(books);
+            Assert.AreEqual(100, actual);
+        }
+
     }
 }
